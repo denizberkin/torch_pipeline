c9e3b96da4c8315b70234b21426862d2e317cea7
denizberkin <berkindeniz2000@gmail.com>
2025-05-27 10:44:21 +0300

    add config strict typechecking

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import os
 
 from losses import *
 from losses.build import build_loss
-from utils.config import check_config_integrity, load_config
+from utils.config import load_config
 
 
 def main():
     conf_folder = os.path.join(os.path.dirname(__file__), "configs")
     config_filenames = [os.path.join(conf_folder, fn) for fn in os.listdir(conf_folder)]
-    config = load_config(config_filenames)
-    config = check_config_integrity(config)
+    config = load_config(config_filenames, strict=True)
+    for lossfn in config.losses:
+        print(f"Loss function: {lossfn.name}, weight: {lossfn.weight}")
 
 
 if __name__ == "__main__":
--- utils/config.py
+++ utils/config.py
@@ -1,11 +1,12 @@
-from typing import List, Union
+from dataclasses import fields, is_dataclass
+from typing import List, Union, get_args, get_origin, get_type_hints
 
 import omegaconf as oc
 
 from utils.schema import ConfigSchema
 
 
-def load_config(file_path: Union[str, List[str]], strict: bool = False) -> oc.DictConfig:
+def load_config(file_path: Union[str, List[str]], strict: bool = False) -> Union[oc.DictConfig, ConfigSchema]:
     """
     Load a configuration file and return it as an omegaconf DictConfig object.
     :param file_path (str or List[str]): Path to the configuration file(s).
@@ -28,4 +29,33 @@ def add_strict_checking(config: oc.DictConfig) -> oc.DictConfig:
     Throws an error if config does not match the schema.
     """
     schema = oc.OmegaConf.structured(ConfigSchema)
-    return oc.OmegaConf.merge(schema, config)
+    merged = oc.OmegaConf.merge(schema, config)
+    validate_keys(ConfigSchema, merged)
+    return merged
+
+
+def validate_keys(schema, merged: oc.DictConfig, path: str = "") -> None:
+    for field in fields(schema):
+        key = field.name
+        sub_path = f"{path}.{key}" if path else key
+        type_hint = get_type_hints(schema).get(key)  # mostly == obj.__annotations__
+        is_optional = get_origin(type_hint) is Union and type(None) in get_args(type_hint)  # check if field is optional
+        if key not in merged:
+            if not is_optional:
+                raise KeyError(f"Missing required key '{sub_path}' in config.")
+            continue  # skip optional keys, no need to check further
+
+        val = merged[key]
+        if is_dataclass(type_hint) and isinstance(val, oc.DictConfig):
+            validate_keys(type_hint, val, sub_path)
+        elif is_list_of_dataclass(type_hint):
+            item_type = get_args(type_hint)[0]
+            for i, item in enumerate(val):
+                if isinstance(item, oc.DictConfig):
+                    validate_keys(item_type, item, f"{sub_path}[{i}]")
+
+
+def is_list_of_dataclass(type_hint):
+    origin = get_origin(type_hint)
+    args = get_args(type_hint)
+    return origin == list and len(args) == 1 and is_dataclass(args[0])
