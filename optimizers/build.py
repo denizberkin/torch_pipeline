dae1b6b0b2ba11aa2f374993cd6acb39f51c0569
denizberkin <berkindeniz2000@gmail.com>
2025-07-22 22:41:02 +0300

    optimizer fixes, TODO: add more pls...

--- optimizers/build.py
+++ optimizers/build.py
@@ -1,6 +1,7 @@
-from typing import Iterator, Union
+"""TODO: add more optimizers, predefined and/or custom"""
+from typing import List
 
-import torch
+import torch.nn as nn
 import torch.optim as optim
 
 from utils.schema import OptimConfig
@@ -8,15 +9,15 @@ from utils.logger import get_logger
 
 
 def build_optimizers(
-    model_params: Iterator[torch.Tensor],
+    models: nn.Module | List[nn.Module],
     config: OptimConfig,
-) -> optim.Optimizer:
+) -> optim.Optimizer | List[optim.Optimizer]:
     """
-    Build optimizers based on the provided configuration.
-    ### Parameters
+    Build optimizers based on the provided configuration and model(s).
+    ### Parameters:
         - config: Configuration dictionary containing optimizer settings.
-    ### Returns
-        - An instance of the optimizer.
+    ### Returns:
+        Instance(s) of the optimizer depending on the number of models provided.
     """
     logger = get_logger()
     optim_class = PREDEFINED_OPTIMIZERS.get(config.name, None)
@@ -24,7 +25,8 @@ def build_optimizers(
         logger.error(f"Optimizer class '{config.name}' not found in predefined optimizers. No custom optim yet.")
         raise ValueError(f"Optimizer class '{config.name}' not found in predefined optimizers. No custom optim yet.")
     logger.info(f"Using class '{config.name}', module name: {__name__}")
-    return optim_class(model_params, config.lr, **config.kwargs)
+    if isinstance(models, list): return [optim_class(m.parameters()) for m in models]
+    return optim_class(models.parameters(), config.lr, **config.kwargs)
 
 
 PREDEFINED_OPTIMIZERS = {
